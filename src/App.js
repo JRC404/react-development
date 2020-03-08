ce6e9df4e53074b74fef86ed0b4c9993b4e46dbb
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-03-08 10:54:38 +0000

    Navbar component functionality added.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
+import Navbar from './Navbar/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Hello, I am a React App</h1>
+        <Navbar />
         <Person />
       </div>
     )
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const navbar = () => {
+    return <p> I will soon be a navbar </p>
+};
+
+export default navbar;
